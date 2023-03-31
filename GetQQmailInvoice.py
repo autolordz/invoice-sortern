03ce79f63331730a0fc3cecd1b913282c5d75d1f
Autoz.Zhao <autolordz@gmail.com>
2023-03-31 16:51:24 +0800

    Update GetQQmailInvoice.py
    添加发票号，添加更多分类，通过读取内容精确判断分类

--- GetQQmailInvoice.py
+++ GetQQmailInvoice.py
@@ -4,7 +4,10 @@ Created on Tue Sep 20 13:26:19 2022
 >> 由头，QQ邮箱发票助手发票整理给财务代理使用，例如每月下载一次发票，发票数量多没能分类，需要分类好方便报销
 
 Updated on Wed Mar 15 10:27:33 2023
->> Update QQ邮箱发票助手里手动每页下载的zip包，放在一个位置，循环解压缩Zip 
+>> QQ邮箱发票助手里手动每页下载的zip包，放在一个位置，循环解压缩Zip 
+
+Updated onMarch 31, 2023, 4:49:01 PM
+>> 添加发票号，添加更多分类，通过读取内容精确判断分类
 
 @author: Autolords
 """
@@ -55,11 +58,14 @@ print('Start Move All'.center(30,'*'))
 except_tag = '芯果科技|优货仓|麦德龙|京东|晶东|宜家|永旺|沃尔玛'
 
 #分类发票,根据自己日常发票添加修改, should be, left (Catalog) = right (Keywords)
-list_tags = dict(餐饮 = '餐饮|饮食|食品|咖啡|快餐|便利店|饮料|酒家|酒楼|中粮',
-     交通 = '交通|地铁|货拉拉|运输',
-     服饰 = '唯品会|迅销|迪卡侬|热风|盖璞',
-     文娱 = '文化|电影|影视',
-     物业管理 = '物业管理|保洁',
+list_tags = dict(餐饮 = '餐饮|饮食|食品|咖啡|快餐|便利店|饮料|酒家|酒楼|豪客来', # 文件名包括上述就不归类到生活百货
+     交通 = '交通|地铁|货拉拉|运输|滴滴|出行|摩拜|骑安',
+     服饰 = '唯品会|迅销|迪卡侬|热风|盖璞|飒拉',
+     文娱 = '文化|电影|影视|娱乐',
+     教育服务 = '培训|考试|教育服务|报名|课程',
+     数码设备 = '数码|设备|电脑|手机|麦克风|耳机|相机|USB|转换器|插座',
+     文具 = '打印纸|笔|文件夹|胶水|回形针|剪刀|纸刀|订书|书桌垫',
+     物业管理 = '物业管理|保洁|垃圾费|租金|水.*电',
      经营 = '体育用品|轴承|包装',
      生活百货 = '药房|%s'%except_tag
      )
@@ -91,7 +97,10 @@ for file_path in glob.glob(os.path.join(file_folder,'*.pdf')):
             firm1 = firms[0].strip()
             if len(firms) > 1:
                 firm2 = firms[1].strip()
-                
+        
+        # 获取发票号码
+        invoice_code = re.search(r'(?<=发票号码[:：]).*|$', t1).group()
+        
         # 获取公司生成 level 1 子文件夹
         folder1 = firm1.strip()[:6]
         
@@ -106,8 +115,15 @@ for file_path in glob.glob(os.path.join(file_folder,'*.pdf')):
         
         # 获取时间生成 level 2 子文件夹
         folder2 = datetime.datetime.strptime(ds1, '%Y年%m月%d日').strftime('%Y %m月')
+        
+        for contentcatalog in ['数码设备','文具','教育服务']:
+            if re.search(r'(%s)|$'%list_tags[contentcatalog], t1).group().strip():
+                break
+            contentcatalog = ''
+        print(contentcatalog)
+        
         # 获取发票类型 level 3 作为文件夹
-        folder3 = get_tag(firm2) if firm2 else get_tag(file_path)
+        folder3 = contentcatalog if contentcatalog else get_tag(firm2) if firm2 else get_tag(file_path)
         
         folder4 = os.path.join(file_folder,folder1,folder2,folder3)
         print(folder4)
@@ -116,12 +132,13 @@ for file_path in glob.glob(os.path.join(file_folder,'*.pdf')):
         os.makedirs(folder4,exist_ok=1)
         
         # 合并成为发票文件名： XX公司-金额-时间.pdf
-        fname = '%s-%s-%s%s'%(
+        fname = '%s-%s-%s-%s%s'%(
             firm2,
             money1,
             ds1,
+            invoice_code,
             os.path.splitext(file_path)[1]
-            ) if not re.search(r'-', file_path) else os.path.basename(file_path)
+            ) 
         
         path2 = os.path.join(file_folder,folder4,fname)
         
